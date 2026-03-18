d2431b0b34677fd6cebe094c8bbbf70e9f3ccbe5
lennart <lennart@ntnu>
2026-03-18 22:34:27 +0100

    authentication(feature):handle post request for /auth/ Checks different errors, such as no @ in email, returns api key if POST request passes all checks
    Need to actually create api key to accepted requests
    
    Simple email check, simple errors such as name and email not being empty

--- internal/handlers/authentication.go
+++ internal/handlers/authentication.go
@@ -3,9 +3,74 @@ package handlers
 import (
 	"crypto/md5"   //for generarting hash to create api key
 	"encoding/hex" //for converting md5 hash to string
+	"encoding/json"
 	"fmt"
-	"time" //for generating unique api key based partly on time hash
+	"net/http"
+	"time" //time of creating api key and for generating unique api key based partly on time hash
 )
+
+type Login struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type key struct {
+	Key       string `json:"key"`
+	CreatedAt string `json:"createdAt"`
+}
+
+func RegisterAuth(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost { //if not a POST request, return method not allowed
+		http.Error(w, "Only POST method allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Parse the JSON body of the request into a Login struct
+	var login Login
+	defer r.Body.Close()
+
+	err := json.NewDecoder(r.Body).Decode(&login)
+	if err != nil {
+		http.Error(w, "Invalid JSON format", http.StatusBadRequest)
+		return
+	}
+
+	// Validate the input (check if name and email are not empty, and if email contains @)
+	if login.Email == "" || login.Name == "" {
+		http.Error(w, "Missing name or email", http.StatusBadRequest)
+		return
+	}
+
+	//check if email contains @, if not, return bad request
+	isAtValid := false
+	for i := 0; i < len(login.Email); i++ {
+		if login.Email[i] == '@' {
+			isAtValid = true
+			break
+		}
+	}
+	if !isAtValid {
+		http.Error(w, "Invalid email format, no @ found", http.StatusBadRequest)
+		return
+	}
+
+	//creating api key
+	createAPI, timeCreateApi := createAPIKey(login.Email)
+
+	//formatting the response to the user, which includes the generated API key and the time of API creation
+	key := key{
+		Key:       createAPI,
+		CreatedAt: timeCreateApi,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(key); err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("API key generated and sent to user:", key.Key)
+
+}
 
 func createAPIKey(email string) (string, string) {
 
@@ -23,5 +88,6 @@ func createAPIKey(email string) (string, string) {
 
 	// Display the character
 	fmt.Println("createAPI:", createAPI)
+
 	return createAPI, timeCreateApi
 }
