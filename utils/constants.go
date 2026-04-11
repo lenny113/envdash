0a0e99ba5b711e334f882d3d45c92993299b5ece
Robin <robinja@stud.ntnu.no>
2026-04-11 18:26:54 +0200

    feat(validation): added constants for validating strings and to get data pre commit there are no constant variables for validation
    post commit there are variables to use for validation and to

--- utils/constants.go
+++ utils/constants.go
@@ -2,3 +2,13 @@ package utils
 
 const VERSION = "v1"
 const REGISTRATION_PATH = "/envdash/" + VERSION + "/registrations/"
+
+const LONGEST_COUNTRYNAME = 56
+const SHORTEST_COUNTRYNAME = 4
+const ISOCODE_LENGTH = 2
+
+const CURRENCYCODE_LENGTH = 3
+const TARGETCURRENCIES_MAX_LENGTH = 10
+
+const CURRENCY_URL = "http://129.241.150.113:9090/currency/AED"
+const COUNTRY_AND_ISO_URL = "http://129.241.150.113:8080/v3.1/all?fields=name,cca2"
